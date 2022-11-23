f99738fe862bd3db5b3f82d98297d3f2c7d52a89
Steve <steveseo.mdk@gmail.com>
2022-11-23 21:37:47 +0900

    20221123

--- 과제 aistore1_1105.py
+++ 과제 aistore1_1105.py
@@ -1,11 +1,5 @@
 class AiStore:
 
-"""
-생성자
-파라미터: 매점이름, 매점아이디, 매점위치
-파라미터 셀프변수에 저장: 매점이름, 매점아이디, 매점위치
-셀프변수 값 초기화: 상품개수, 상품가격
-"""
     def __init__(self, s_name, s_id, s_locate):
         self.s_name = s_name
         self.s_id = s_id
@@ -17,32 +11,20 @@ class AiStore:
         self.p_count = p_count
         self.p_price = p_price
 
-"""
-- 파라미터: 구매개수, 입력금액
-- 상품개수에서 구매개수 차감
-- 상품가격과 구매개수로부터 필요금액 산출
-- 입력금액에서 필요금액 차감하여 '잔돈은 ~입니다' 출력(print)
-- 상품개수가 모자라면 '재고가 부족합니다.' 출력(print)
-- 입력금액이 부족하면 '금액이 부족합니다.' 출력(print)
-"""
-    def buy_item(self, order_unit, input_money):
-        self.order_unit = order_unit
-        self.input_money = input_money
-        self.change_money = 0
-
-        p_count =- order_unit
-        need_price = self.p_price*order_unit
-
-        if self.change_money == input_money - need_price:
-            print('잔돈은', 'change_money', '입니다')
-
-        if self.p_count < order_unit:
+    def buy_item(self, p_count, input_money):
+        if self.p_count < p_count :
             print('재고가 부족합니다')
+            return
+        need_price = self.p_price * count
 
-        if self.input_money < need_price:
+        if input_money < need_price :
             print('금액이 부족합니다')
+            return
 
-
+        self.p_count -= p_count
+        change_money = input_money - need_price
+        print('잔돈은', int(change_money), '입니다')
+        # print('잔돈은 %i입니다' % change_money)
 
     def get_name(self):
         return self.s_name
@@ -68,31 +50,31 @@ if __name__ == '__main__':
 
     store = AiStore(s_name, s_id, s_locate)
 
-    print(store.get_name() + ' 스토어가 생성 되었습니다.')
+    print(store.get_name(), ' 스토어가 생성 되었습니다.')
 
     for i in range(10):
 
         print('스토어 조회는 1번, 구매는 2번, 상품 관리는 3번, 종료는 4번 을 눌러주세요')
         num = input()
         if num =='1':
-            store.get_name()
+            print(store.get_name(), store.get_id(), store.get_locate(), '스토어가 생성되었습니다')
 
         elif num == '2':
-            order_unit = input('구매할 상품 개수 입력')
-            store.buy_item()
-
-            need_price = self.p_price * order_unit
-
-            amount = input(str(need_price) + ' 금액을 입력해 주세요')
-            ~
+            # count = int(input('구매할 상품 개수 입력'))
+            count = int(input('구매할 상품 개수 입력'))
+            need_price = store.p_price * count
+            # amount = int(input(str(need_price) + ' 금액을 입력해 주세요'))
+            amount = int(input(str(need_price) + ' 금액을 입력해 주세요'))
+            store.buy_item(count, amount)
 
         elif num == '3':
-            ~ = input('상품 재고 입력')
-            ~ = input('상품 가격 입력')
-            ~
+            count = input('상품 재고 입력')
+            price = input('상품 가격 입력')
+            store.set_product(count, price)
 
         elif num == '4':
             print('종료합니다')
             break
+
         else:
             print('잘못된 입력입니다')
