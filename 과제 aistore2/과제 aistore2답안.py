554ec26db7cb877073696019db883a237e0722c3
Steve <steveseo.mdk@gmail.com>
2022-12-02 21:39:47 +0900

    20221202

--- 과제 aistore2/과제 aistore2답안.py
+++ 과제 aistore2/과제 aistore2답안.py
@@ -5,33 +5,27 @@ class AiStore:
         self.s_id = s_id
         self.locate = locate
         self.products = {'커피':10}
-        self.prices = {'커피':1000}
+        self.prices = {'커피':2000}
 
-    # 상품 주문 개수를 확인할 수 있어야 한다. 상품 가격을 확인 할 수 있어야 한다.
-    # 없는 상품이면 상품 주문 개수에 할당하지 않는다.
     def set_product(self, product, count, price):
+
         if product in self.products:
             self.products[product] += count
             self.prices[product] = price
-
-        else :
+        else:
             self.products[product] = count
             self.prices[product] = price
-            return
-
-
-    # 구매한 상품 수량이 재고 수량보다 작으면 상품 재고 수량을 차감한다.
-    # 지불 금액에서 상품 가격을 차감하여 잔돈을 계산한다.
 
     def buy_product(self, product, count, amount):
-        if count * self.products[product] < amount:
-            self.products[product] -= count
-            changes = amount - count * self.prices[product]
-            print('잔돈은' + str(changes))
-
-        else :
-            print('금액이 부족합니다')
-            return
+        if count < self.products[product]:
+            if count * self.prices[product] < amount:
+                self.products[product] -= count
+                changes = amount - count * self.prices[product]
+                print('잔돈은 ' + str(changes))
+            else:
+                print('금액이 부족합니다.')
+        else:
+            print('재고가 부족합니다.')
 
 
     def get_name(self):
@@ -53,24 +47,27 @@ def create_store():
     s_name = input('스토어 이름 입력: ')
     s_id = input('스토어 아이디 입력: ')
     locate = input('스토어 위치 입력: ')
-
     store = AiStore(s_name, s_id, locate)
-
     print('{} 스토어가 생성 되었습니다.'.format(store.get_name()))
     return store
 
 
 def show_list():
     for store in store_list:
-        print('스토어 이름:{} 스토어 아이디:{} 스토어 위치:{}'.format(store.get_name(), store.get_id(), store.get_products()))
-
+        print('스토어 이름:{} 스토어 아이디:{} 스토어 위치:{}'
+              .format(store.get_name(),
+                      store.get_id(),
+                      store.get_locate()
+                      ))
 
 def search_store(s_id):
     for store in store_list:
-        if store.get_id == s_id:
+        if store.get_id() == s_id:
             return store
-        print('스토어 아이디를 찾지 못했습니다.')
-        return None
+
+    print('스토어 아이디를 찾지 못했습니다.')
+    return None
+
 
 def show_store():
     s_id = input('스토어 아이디 입력: ')
@@ -78,9 +75,14 @@ def show_store():
     if store is None:
         return
 
-    print('{} 스토어 재고 현황: {}'.format(store.get_products()))
-    print('{} 스토어 가격 현황: {}'.format(store.get_price()))
-
+    print('{} 스토어 재고 현황: {}'
+          .format(store.get_name(),
+                  store.get_products()
+                  ))
+    print('{} 스토어 가격 현황: {}'
+          .format(store.get_name(),
+                  store.get_prices()
+                  ))
 
 def buy():
     s_id = input('스토어 아이디 입력: ')
@@ -88,18 +90,16 @@ def buy():
     if store is None:
         return
 
-    product = input('상품 입력: ')
+    product = input('상품 입력:')
     count = input('구매 개수 입력: ')
-    # 옵션 '총 가격은 {} 입니다.' 출력
-
+    count = int(count)
+    # 옵션
     if product in store.get_products():
-        print('총 가격은 {} 입니다'.format(store.get_prices()[product] * count))
+        print('총 가격은 {} 입니다.'.format(store.get_prices()[product] * count))
 
     price = input('가격 입력: ')
     price = int(price)
-
     store.buy_product(product, count, price)
-
 
 def manager_product():
 
@@ -112,8 +112,7 @@ def manager_product():
     count = input('재고 개수 입력: ')
     price = input('상품 가격 입력: ')
 
-    store.set_product(product, count, price)
-
+    store.set_product(product, int(count), int(price))
 
 def txt_to_store():
     path = input('파일 경로입력: ')
@@ -125,7 +124,6 @@ def txt_to_store():
             store_list.append(
                 AiStore(sdata[0].strip(), sdata[1].strip(), sdata[2].strip())
             )
-    pass
 
 
 import json
@@ -136,21 +134,19 @@ def store_to_json():
 
     for s in store_list:
         s_dict = {
-            'name' : s.get_name(),
-            's_id' : s.get_id(),
-            'locate' : s.get_locate(),
-            'product' : s.get_products(),
-            'price' : s.get_price()
+            'name': s.get_name(),
+            's_id': s.get_id(),
+            'locate': s.get_locate(),
+            'products': s.get_products(),
+            'prices': s.get_prices()
         }
         json_data.append(s_dict)
 
-    with open(open, 'w') as f:
-        json.dump(json_data, f)
+    with open(path, 'w') as f:
+        json.dump(json_data,f)
 
-    f.closse()
+    f.close()
     print(json_data)
-    pass
-
 
 if __name__ == '__main__':
     store_list = []
@@ -171,18 +167,16 @@ if __name__ == '__main__':
             store = create_store()
             store_list.append(store)
         elif input1 == '2':
-            store = show_list()
+            show_list()
         elif input1 == '3':
-            store = show_store()
+            show_store()
         elif input1 == '4':
-            store = buy()
+            buy()
         elif input1 == '5':
-            store = manager_product()
+            manager_product()
         elif input1 == '6':
             txt_to_store()
-            pass
         elif input1 == '7':
             store_to_json()
-            pass
         else:
             print('존재하지 않는 명령어 입니다.')
